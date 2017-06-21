36e919263bf7d8aaa08919337d30e1750b8e4815
lanletter <lanletter@qq.com>
2017-06-21 16:21:52 +0800

    在fotile-2017-06-15的基础上修改了直播初始化图片

---
+++ js/lifedetail.js
@@ -0,0 +1,41 @@
++ function($) {
+  $(function() {
+    var $header = $('.header'),
+      $content = $('.content');
+    // var url = 'http://121.40.135.115:8080/fotile-api-0.0.2/test/wantFeel/detail';
+
+    $.ajax({
+      url: 'http://121.40.135.115:8080/fotile-api-0.0.2/test/wantFeel/detail',
+      type: 'POST', //GET
+      data: JSON.stringify({ "userName": "test", "address": "gz", id: 123 }),
+      timeout: 5000, //超时时间
+      contentType: "application/json;charset=utf-8",
+      dataType: 'json', //返回的数据格式：json/xml/html/script/jsonp/text
+      success: function(data, textStatus, jqXHR) {
+        callback(data);
+      },
+      error: function(xhr, textStatus) {
+        console.log('错误')
+        console.log(JSON.stringify(xhr))
+        console.log(JSON.stringify(textStatus))
+      }
+    })
+
+    function callback(res) {
+      if (res.status !== 200) {
+        alert(res.errorMessage)
+        return;
+      }
+      var data = res.data;
+      $header.find('.header__main h3').text(data.longTitle || data.shortTitle || ' ');
+      $api.imgAdapt($header.find('.header__main .img'), data.coverPicture.path)
+      $header.find('.view-count strong').text(data.viewCount);
+      $header.find('.comment-count strong').text(data.commentCount);
+      $header.find('.favorite-count strong').text(data.favoriteCount);
+
+      $content.append(data.content);
+    }
+
+    $.loadComment({"pageNum":1,"pageSize":5,"refId":434,"type":5,"userId":81177});
+  });
+}(jQuery);
---
+++ js/loadcomment.js
@@ -0,0 +1,54 @@
++function() {
+  function loadComment(obj) {
+    $api.post('http://121.40.135.115:8080/fotile-api-0.0.2/comment/list', obj, 
+      function(res) {
+        callback(res);
+      }
+    )
+    function callback (res) {
+      var data = res.data;
+      if(data.length == 0) {
+        var noComment = '<div class="ft-comment__header clearfix left-right">\
+          <span class="left">评论 <i></i></span>\
+          <a href="#""><span class="right comment"></span></a>\
+        </div><p>暂无评论，快去抢沙发吧</p>';
+        $('.ft-comment').append($(noComment));
+        return;
+      }
+      var template = '<div class="ft-comment__header clearfix left-right">\
+          <span class="left">评论 <i>' + data.length + '</i></span>\
+          <a href="#""><span class="right comment"></span></a>\
+        </div>\
+        <ul>\
+          {{#data}}\
+          <li class="ft-comment__content left-right clearfix">\
+            <img src="{{userInfomation.titlePicture}}" class="header-pic left">\
+            <p class="right">\
+              <span class="title-box clearfix left-right">\
+                <span class="left name">{{createby}}</span>\
+                <span class="right time"> <time>{{createat}}</time></span>\
+              </span>\
+              <span class="text">\
+                {{content}}\
+              </span>\
+              {{#commentPictureList}}\
+               <img src="{{picture.path}}">\
+              {{/commentPictureList}}\
+              <button class="reply">回复TA</button>\
+              <button class="more">查看更多</button>\
+            </p>\
+          </li>\
+          {{/data}}\
+        </ul>';
+
+      res.data.forEach(function(item, index) {
+        item.createat = new Date(item.createat).app();
+      });
+      Mustache.parse(template);
+      var rendered = Mustache.render(template, res);
+      $('.ft-comment').append($(rendered));
+    }
+  }
+
+  $.loadComment = loadComment;
+}();
