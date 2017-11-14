ab9f7d0c4dff6a16d642fc1c6c989ebbaf9eb173
NaiHeWangChuan <304893874@qq.com>
2017-11-14 16:25:26 +0800

    confirm

--- js/confirm.js
+++ js/confirm.js
@@ -1,16 +1,22 @@
-$(".delete").onclick = function() {
-    var div = document.createElement("div");
-    div.innerHTML =
-        '<div id="mask" class=" works-mask">' +
+var confirm=function () {
+    $("body").append(
+        '<div id="mask" class="mask works-mask">' +
         '<div class="mask-content">' +
         '<p class="del-p">您确定要删除图片吗？</p>' +
         '<p class="check-p">' +
         '<span class="del-com wsdel-ok">确定</span>' +
         '<span class="wsdel-no">取消</span>' +
-        '</p></div></div>';
-    document.body.appendChild(div);
-
-    var mask = document.getElementById("mask");
-    mask.style.display = 'block';
-
+        '</p></div></div>');
+    $("#mask").css("display","block");
+    $(".wsdel-ok").click(function () {
+        $("#mask").css("display","none");
+        $this.userId = userid;
+        $this.getAddress(this.userId);
+    });
+    $(".wsdel-no").click(function () {
+        $("#mask").css("display","none");
+    })
 };
+
+
+
