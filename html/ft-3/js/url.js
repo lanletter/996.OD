228e5c0e94a049d7794c47c041e304f0c9dbb005
lanletter <lanletter@qq.com>
2018-02-10 15:33:33 +0800

    2月10日，新增落地页，其他页面版本号修改

--- html/ft-3/js/url.js
+++ html/ft-3/js/url.js
@@ -1,3 +1,3 @@
-var urlport="http://121.40.135.115:8080/fotile-api-0.0.2/";
-// var urlport="https://api.fotilestyle.com/fotile-api-0.0.2/";
+// var urlport="http://121.40.135.115:8080/fotile-api-0.0.2/";
+var urlport="https://api.fotilestyle.com/fotile-api-0.0.2/";
 
--- html/login/js/url.js
+++ html/login/js/url.js
@@ -1,5 +1,5 @@
 /*正式环境*/
-// var urlport="https://api.fotilestyle.com/fotile-api-0.0.2/";
+var urlport="https://api.fotilestyle.com/fotile-api-0.0.2/";
 /*测试环境*/
-var urlport="http://121.40.135.115:8080/fotile-api-0.0.2/";
+// var urlport="http://121.40.135.115:8080/fotile-api-0.0.2/";
 
