9fe67a46f9fdd5b8ae24bfc041a1b79714e88033
lanletter <lanletter@qq.com>
2018-03-26 10:45:35 +0800

    3月12日，预发布环境接口地址。

--- js/url.js
+++ js/url.js
@@ -3,11 +3,11 @@
 // var urlport2="https://h5.fotilestyle.com/";
 // var urlport3="https://api.fotilestyle.com/fotile-api-0.0.2/";
 /*预发布环境*/
-// var urlport="http://47.97.37.120:666/fotile-api-0.0.2/";
-// var urlport2="http://h5.fotilestyle.com/"; //研究院验证码页面专用
-// var urlport3="http://114.55.39.95:666/fotile-api-0.0.2/";
-/*测试环境*/
-var urlport="http://121.40.135.115:8080/fotile-api-0.0.2/";
+var urlport="http://47.97.37.120:666/fotile-api-0.0.2/";
 var urlport2="http://h5.fotilestyle.com/"; //研究院验证码页面专用
 var urlport3="http://114.55.39.95:666/fotile-api-0.0.2/";
+/*测试环境*/
+// var urlport="http://121.40.135.115:8080/fotile-api-0.0.2/";
+// var urlport2="http://h5.fotilestyle.com/"; //研究院验证码页面专用
+// var urlport3="http://114.55.39.95:666/fotile-api-0.0.2/";
 
