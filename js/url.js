f393ed8e2aafd97b9e8a329b9c0c281745421892
lanletter <lanletter@qq.com>
2018-06-11 14:42:50 +0800

    V430上线后存档

--- js/url.js
+++ js/url.js
@@ -1,13 +1,13 @@
 /*正式环境*/
-//var urlport="https://api.fotilestyle.com/fotile-api-0.0.2/";
-//var urlport2="https://h5.fotilestyle.com/";
-//var urlport3="https://api.fotilestyle.com/fotile-api-0.0.2/";
+// var urlport="https://api.fotilestyle.com/fotile-api-0.0.2/";
+// var urlport2="https://h5.fotilestyle.com/";
+// var urlport3="https://api.fotilestyle.com/fotile-api-0.0.2/";
 /*预发布环境*/
 // var urlport = "http://apppre.fotilestyle.com:666/fotile-api-0.0.2/";
 // var urlport2 = "http://h5.fotilestyle.com/"; //研究院验证码页面专用
 // var urlport3 = "http://114.55.39.95:666/fotile-api-0.0.2/";
 /*测试环境*/
-   var urlport = "http://apptest.fotilestyle.com:666/fotile-api-0.0.2/";
-   var urlport2 = "http://h5.fotilestyle.com/"; //研究院验证码页面专用
-   var urlport3 = "http://114.55.39.95:666/fotile-api-0.0.2/";
+var urlport = "http://apptest.fotilestyle.com:666/fotile-api-0.0.2/";
+var urlport2 = "http://h5.fotilestyle.com/"; //研究院验证码页面专用
+var urlport3 = "http://114.55.39.95:666/fotile-api-0.0.2/";
 
