e9f8f808021eaa9254f04ff3ac8a1ae7889eaa1e
lanletter <lanletter@qq.com>
2018-06-29 17:12:55 +0800

    深度链接test

--- js/deeplink.js
+++ js/deeplink.js
@@ -129,15 +129,21 @@
                 /*深链接打开*/
                 var deeplinkurl = "fotile://api.fotilestyle.com/" + urlright;
                 // alert(deeplinkurl);
-                window.location.href = deeplinkurl;
                 /***打开app的协议***/
-                window.setTimeout(function () {
-                    if (isiOS == true) {/*ios手机*/
-                        window.location.href = "https://gio.ren/re0Y923";
-                    } else if (isAndroid == true) {/*Android手机*/
-                        window.location.href = "https://gio.ren/rBZ2ApB";
-                    }
-                }, 1000);
+                window.location.href = deeplinkurl;
+                /***打开app失败后***/
+                // var t = setTimeout(function() {
+                //     if (isiOS == true) {/*ios手机*/
+                //         window.location.href = "https://gio.ren/re0Y923";
+                //     } else if (isAndroid == true) {/*Android手机*/
+                //         window.location.href = "https://gio.ren/rBZ2ApB";
+                //     }
+                // }, 1000);
+                // /*跳转成功后阻止浏览器继续跳转*/
+                // $(window).blur(function(){
+                //     clearTimeout(t);
+                //     return;
+                // });
             }
 
         }
