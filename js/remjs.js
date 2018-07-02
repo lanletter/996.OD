8c3321c504d555bc9f50c088cb90733da6a3038d
lanletter <lanletter@qq.com>
2018-07-02 17:18:28 +0800

    删除废弃文件

--- js/remjs.js
+++ js/remjs.js
@@ -1,19 +1,19 @@
 // rem 适配手机屏幕
 ;(function (win) {
-	var doc = win.document,
-		docEle = doc.documentElement;
-	var tid;
-	document.body.style.opacity = 0;
+    var doc = win.document,
+        docEle = doc.documentElement;
+    var tid;
+    document.body.style.opacity = 0;
 
-	function refreshRem () {
-		var width = docEle.getBoundingClientRect().width
-		if (width > 750) width = 750;
-		var rem = width / 7.5;
-		docEle.style.fontSize = rem + 'px';
-		document.body.style.opacity = 1;
-	}
+    function refreshRem () {
+        var width = docEle.getBoundingClientRect().width
+        if (width > 750) width = 750;
+        var rem = width / 7.5;
+        docEle.style.fontSize = rem + 'px';
+        document.body.style.opacity = 1;
+    }
 
-	win.addEventListener('resize', function() {
+    win.addEventListener('resize', function() {
         clearTimeout(tid);
         tid = setTimeout(refreshRem, 500);
     }, false);
@@ -23,5 +23,5 @@
             tid = setTimeout(refreshRem, 500);
         }
     }, false);
-	setTimeout(refreshRem, 500);
+    setTimeout(refreshRem, 500);
 })(window)
