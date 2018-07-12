709985b6d7be6477c65c67d5f355d2b86a498b2b
lanletter <lanletter@qq.com>
2018-07-12 15:15:04 +0800

    评论上传图片提示test

--- js/imgUp.js
+++ js/imgUp.js
@@ -165,7 +165,7 @@
                     var formdata = new FormData();
                     var filestring = e.target.result;   //base64编码的图片数据
                     console.log(filestring);
-                    alert(filestring);
+                    // alert(filestring);
                     formdata.append("type", 2);
                     formdata.append("image", filestring);
                     var json = {};
@@ -197,7 +197,7 @@
                             }
                         },
                         error: function (e) {
-                            $("#waiting").remove();
+                            // $("#waiting").remove();
                             console.log(e);
                             // alert(JSON.stringify(e));
                             // obj.remove();
