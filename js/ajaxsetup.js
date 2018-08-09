5ceb800bd068b287dd137db2b76e19fccf88fa64
lanletter <lanletter@qq.com>
2018-08-09 17:56:30 +0800

    增加token校验

--- js/ajaxsetup.js
+++ js/ajaxsetup.js
@@ -1,11 +1,29 @@
 $(function () {
+    //读取cookies
+    function getCookie(name)
+    {
+        var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+
+        if(arr=document.cookie.match(reg))
+
+            return unescape(arr[2]);
+        else
+            return null;
+    }
+    var userid = getCookie("userid");
+    var token = getCookie("token");
+    // console.log({ // 默认添加请求头
+    //     "userId": userid,
+    //     "token": token,
+    //     "deviceType": "web",
+    //     "version":"v437"
+    // });
     $.ajaxSetup({
         headers: { // 默认添加请求头
-            "userId":userid,
-            "tel":tel,
-            "Access-Token": token,
-            "deviceType":web,
-            "version":v421
+            "userId": userid,
+            "token": token,
+            "deviceType":"web",
+            "version":"v437"
         }
     });
 });
