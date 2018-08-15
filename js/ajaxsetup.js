839106bfd885f96f0849c0d876192849cb824c2d
lanletter <lanletter@qq.com>
2018-08-15 17:21:20 +0800

    436版请求头

--- js/ajaxsetup.js
+++ js/ajaxsetup.js
@@ -1,28 +1,7 @@
 $(function () {
     //读取cookies
-    function getCookie(name)
-    {
-        var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
-
-        if(arr=document.cookie.match(reg))
-
-            return unescape(arr[2]);
-        else
-            return null;
-    }
-    var userid = getCookie("userid");
-    var token = getCookie("token");
-    // console.log({ // 默认添加请求头
-    //     "userId": userid,
-    //     "token": token,
-    //     "deviceType": "web",
-    //     "version":"v437"
-    // });
     $.ajaxSetup({
-        headers: { // 默认添加请求头
-            "userId": userid,
-            "token": token,
-            "deviceType":"web",
+        headers: { // 添加默认请求头
             "version":"v436"
         }
     });
